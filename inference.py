58e28fedfa42afc27ad17f39e5decdb1a11cacf5
chunglu-chou <johnnie.zhou@gmail.com>
2021-07-23 21:34:30 +0800

    add: connect backend js to python inference program

--- inference.py
+++ inference.py
@@ -1,13 +1,14 @@
+import json
 import numpy as np
 import os
 import pickle
+import sys
 
 modelPath = os.getcwd() + "/model-1627044496.525413.sav"
 
-fakeData = np.array([[0.00632, 18, 2.31, 0, 0.538, 6.575,
-                     65.2, 4.09, 1, 296, 15.3, 396.9, 4.98]], dtype = np.float64)
-
-with open(modelPath, 'rb') as modelFile:
-    model = pickle.load(modelFile)
-    prediction = model.predict(fakeData)[0]
-    print(prediction)
+if __name__ == "__main__":
+    with open(modelPath, 'rb') as modelFile:
+        fakeData = json.load(sys.stdin)
+        model = pickle.load(modelFile)
+        prediction = model.predict(fakeData)[0]
+        json.dump(prediction, sys.stdout)
